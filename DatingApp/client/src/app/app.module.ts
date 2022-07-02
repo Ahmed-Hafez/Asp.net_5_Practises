b7fdf4040f31a8f8afb2a6f62bf1f2b96ce61a83
Ahmed Hafez <41956720+Ahmed-Hafez@users.noreply.github.com>
2022-07-02 21:34:56 +0200

    Handling errors in both frontend and backend

--- DatingApp/client/src/app/app.module.ts
+++ DatingApp/client/src/app/app.module.ts
@@ -1,9 +1,10 @@
+import { ErrorInterceptor } from './interceptors/error/error.interceptor';
 import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,9 +14,18 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './shared/components/home/home.component';
 import { RegisterComponent } from './shared/components/register/register.component';
 import { ToastrModule } from 'ngx-toastr';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -24,7 +34,9 @@ import { ToastrModule } from 'ngx-toastr';
     FormsModule,
     SharedModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ DatingApp/client/src/app/interceptors/error/error.interceptor.ts
@@ -0,0 +1,69 @@
+import { ToastrService } from 'ngx-toastr';
+import { NavigationExtras, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private router: Router, private toastr: ToastrService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((errorResponse: HttpErrorResponse) => {
+        if (errorResponse) {
+          switch (errorResponse.status) {
+            case 400:
+              if (errorResponse.error.errors) {
+                const modalStateErrors = [];
+                for (const error of errorResponse.error.errors) {
+                  modalStateErrors.push(error);
+                }
+
+                throw modalStateErrors.flat();
+              } else {
+                this.toastr.error(
+                  errorResponse.statusText,
+                  errorResponse.status.toString()
+                );
+              }
+              break;
+
+            case 401:
+              this.toastr.error(
+                errorResponse.statusText,
+                errorResponse.status.toString()
+              );
+              break;
+
+            case 404:
+              this.router.navigateByUrl('/not-found');
+              break;
+
+            case 500:
+              const navigationExstras: NavigationExtras = {
+                state: { error: errorResponse.error },
+              };
+              this.router.navigateByUrl('/server-error', navigationExstras);
+              break;
+
+            default:
+              this.toastr.error('Something unexpected went wrong');
+              console.error(errorResponse);
+              break;
+          }
+        }
+        return throwError(() => errorResponse);
+      })
+    );
+  }
+}
